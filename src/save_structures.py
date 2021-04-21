369b40559e9bac24cc3791e92d01c8a780175c9a
Dexter Antonio <dexter.d.antonio@gmail.com>
2021-04-21 16:15:05 -0700

    added docstrings

--- src/save_structures.py
+++ src/save_structures.py
@@ -4,11 +4,26 @@ from pathlib import Path
 import yaml
 from ase.db import connect
 from ase.io import read, write
-from fireworks import  LaunchPad
+from fireworks import LaunchPad
 
 
 class SaveStructures:
-    def __init__(self, db_path: str, launchpad_path: str, output_filename=None, fireworks_dict_path=None):
+    """
+    Fireworks added optimized structures to a database. This class then loads the optimized structures
+    from that database and saves them in the original file structure from which they originated.
+    """
+    def __init__(self, db_path: str, launchpad_path: str, output_filename='optimized.traj', fireworks_dict_path=None):
+        """
+        This initializes the save structures class
+        :param db_path: path to the database (can be an AWS connection string)
+        :type db_path: str
+        :param launchpad_path: path to fireworks launchpad
+        :type launchpad_path: str
+        :param output_filename: the name of the filename in which to save the output (default 'optimized.traj')
+        :type output_filename: str
+        :param fireworks_dict_path: path to dictionary mapping indices to structures
+        :type fireworks_dict_path: str
+        """
         self.db = connect(db_path)
         if not fireworks_dict_path:
             self.fireworks_dict_path = 'fireworks_dict.json'
@@ -20,16 +35,17 @@ class SaveStructures:
                                             object_hook=lambda d: {int(k) if k.lstrip('-').isdigit() else k: v for k, v
                                                                    in d.items()})
 
-        if output_filename is None:
-            self.new_filename = 'optimized.traj'
-        else:
-            self.new_filename = output_filename
-
         with open(launchpad_path) as file:
             con_params = yaml.load(file, Loader=yaml.FullLoader)
         self.launchpad = LaunchPad(**con_params)
 
-    def save(self):
+    def save(self) -> None:
+        """
+        Attempts to save the optimized structures to the folder structure.
+        Errors are not thrown, but instead printed.
+        :return: None
+        :rtype: None
+        """
         for filename, fireworks in self.fireworks_dict.values():
             new_filename = Path(filename).parents[0] / self.new_filename
             final_structure_id = \
